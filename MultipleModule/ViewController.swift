6a72785be3916ed1f29a3a7b5c450715c7a17ea8
Hieu Nguyen Pham Minh <hieu.nguyenphamminh@thecoffeehouse.vn>
2025-02-11 21:17:30 +0700

    Initial Commit

---
+++ MultipleModule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultipleModule
+//
+//  Created by HieuNguyen on 11/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
